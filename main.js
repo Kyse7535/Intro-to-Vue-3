0aaf323af457a0c2b2f3011b6ea3f898bc63d8ad
Kyse7535 <ktevot@gmail.com>
2022-07-23 07:36:50 +0200

    add features

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: {
+                description: "This is my description",
+                name: 'Socks'
+            }
+        }
+    }
+})
